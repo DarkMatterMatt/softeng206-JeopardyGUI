fec1cd25025bce5236be05c2cbb4642c3ccec38f
Matt Moran <15150339+DarkMatterMatt@users.noreply.github.com>
2020-08-27 11:36:27 +1200

    Improve clickable area for question
    The question's polygon is clickable, not the question's container

--- src/se206/a2/QuestionView.java
+++ src/se206/a2/QuestionView.java
@@ -15,14 +15,15 @@ public class QuestionView {
                 topWidth, 0,
                 bottomLeftX + bottomWidth, height,
                 bottomLeftX, height);
-        background.getStyleClass().add("background");
+        background.getStyleClass().addAll("background", "btn");
+        background.setOnMouseClicked(ev -> model.askQuestion(question));
 
         Label valueLabel = new Label("$" + question.getValue());
         valueLabel.getStyleClass().add("value");
+        valueLabel.setMouseTransparent(true);
 
-        _container.getStyleClass().addAll("question", "btn");
+        _container.getStyleClass().add("question");
         _container.getChildren().addAll(background, valueLabel);
-        _container.setOnMouseClicked(ev -> model.askQuestion(question));
 
         updateStatus(question.getStatus());
         question.getStatusProperty().addListener((observable, oldVal, newVal) -> updateStatus(newVal));
