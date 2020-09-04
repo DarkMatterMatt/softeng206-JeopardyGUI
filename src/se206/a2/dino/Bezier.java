124525f52ab4f555d458b5226b2e531921f5dc18
Matt Moran <15150339+DarkMatterMatt@users.noreply.github.com>
2020-09-04 20:37:12 +1200

    Add cubic bezier calculator

---
+++ src/se206/a2/dino/Bezier.java
@@ -0,0 +1,80 @@
+package se206.a2.dino;
+
+import java.awt.geom.Point2D;
+
+public class Bezier {
+    public static class Cubic {
+        private final double _p1X, _p1Y, _p2X, _p2Y, _aX, _bX, _cX, _aY, _bY, _cY;
+
+        /**
+         * Create a new Cubic Bezier curve.
+         *
+         * @param p1X x-coordinate for the first point on the curve
+         * @param p1Y y-coordinate for the first point on the curve
+         * @param p2X x-coordinate for the second point on the curve
+         * @param p2Y y-coordinate for the second point on the curve
+         */
+        public Cubic(double p1X, double p1Y, double p2X, double p2Y) {
+            _p1X = p1X;
+            _p1Y = p1Y;
+            _p2X = p2X;
+            _p2Y = p2Y;
+
+            _cX = 3 * _p1X;
+            _bX = 3 * (_p2X - _p1X) - _cX;
+            _aX = 1 - _cX - _bX;
+
+            _cY = 3 * _p1Y;
+            _bY = 3 * (_p2Y - _p1Y) - _cY;
+            _aY = 1 - _cY - _bY;
+        }
+
+        /**
+         * @param t time 0 <= t <= 1
+         * @return progress at time `t`
+         */
+        public double get(double t) {
+            return getY(t);
+        }
+
+        public double getP1X() {
+            return _p1X;
+        }
+
+        public double getP1Y() {
+            return _p1Y;
+        }
+
+        public double getP2X() {
+            return _p2X;
+        }
+
+        public double getP2Y() {
+            return _p2Y;
+        }
+
+        /**
+         * @param t time 0 <= t <= 1
+         * @return point on curve at time `t`
+         */
+        public Point2D getPoint(double t) {
+            return new Point2D.Double(getX(t), getY(t));
+        }
+
+        /**
+         * @param t time 0 <= t <= 1
+         * @return x-value on curve at time `t`
+         */
+        public double getX(double t) {
+            return (_aX * Math.pow(t, 3)) + (_bX * Math.pow(t, 2)) + (_cX * t);
+        }
+
+        /**
+         * @param t time 0 <= t <= 1
+         * @return y-value on curve at time `t`
+         */
+        public double getY(double t) {
+            return (_aY * Math.pow(t, 3)) + (_bY * Math.pow(t, 2)) + (_cY * t);
+        }
+    }
+}
---
+++ src/se206/a2/dino/BezierTest.java
@@ -0,0 +1,41 @@
+package se206.a2.dino;
+
+import org.junit.Test;
+
+import java.awt.geom.Point2D;
+
+import static org.junit.Assert.assertEquals;
+
+public class BezierTest {
+    Bezier.Cubic cubic1 = new Bezier.Cubic(0.17, 0.67, 0.83, 0.67);
+
+    @Test
+    public void cubic_hasCorrectValueAt000() {
+        pointEquals(cubic1.getPoint(0.00), 0, 0);
+    }
+
+    @Test
+    public void cubic_hasCorrectValueAt024() {
+        pointEquals(cubic1.getPoint(0.24), 0.19352, 0.38045);
+    }
+
+    @Test
+    public void cubic_hasCorrectValueAt072() {
+        pointEquals(cubic1.getPoint(0.72), 0.76346, 0.77846);
+    }
+
+    @Test
+    public void cubic_hasCorrectValueAt098() {
+        pointEquals(cubic1.getPoint(0.98), 0.98922, 0.98059);
+    }
+
+    @Test
+    public void cubic_hasCorrectValueAt100() {
+        pointEquals(cubic1.getPoint(1.00), 1, 1);
+    }
+
+    private void pointEquals(Point2D p, double x, double y) {
+        assertEquals(x, p.getX(), 1e-5);
+        assertEquals(y, p.getY(), 1e-5);
+    }
+}
