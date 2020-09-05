a1d7c7c73628f16b2bcb0580afe161a846e986ce
Matt Moran <15150339+DarkMatterMatt@users.noreply.github.com>
2020-09-06 11:26:57 +1200

    Remove firstTick detection
    firstTick is already handled by DinoModel

--- src/se206/a2/dino/Background.java
+++ src/se206/a2/dino/Background.java
@@ -6,24 +6,20 @@ import javafx.scene.layout.HBox;
 
 import java.awt.*;
 
+/**
+ * Infinite scrolling background
+ */
 public class Background extends GameObject {
-    private final Image _image = new Image(GameObjectFactory.class.getResourceAsStream("assets/background.png"));
-    private boolean _firstTick = true;
+    private static final Image _image = new Image(GameObjectFactory.class.getResourceAsStream("assets/background.png"));
 
     public Background() {
         super(new Rectangle(4800, 24), new HBox());
         getView().setViewOrder(10);
+        ((HBox) getImage()).getChildren().addAll(new ImageView(_image), new ImageView(_image));
     }
 
     @Override
     protected void onTick(double secs, double runningSpeed) {
-        // background loads after first game tick so that the scene doesn't
-        //   automatically expand to try and fit the entire width of the background
-        if (_firstTick) {
-            _firstTick = false;
-            ((HBox) getImage()).getChildren().addAll(new ImageView(_image), new ImageView(_image));
-        }
-
         // when one side goes off the screen, shift ourselves right
         if (getX() < -2400) {
             setX(getX() + 2400);
